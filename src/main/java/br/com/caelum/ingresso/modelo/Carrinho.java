c9bddddb3ff2402d139eb4bfa5c793eb330fbb6d
Felipe Castro <felipe_fcastro@yahoo.com.br>
2017-09-01 22:25:57 -0300

    ultimo commit

--- src/main/java/br/com/caelum/ingresso/modelo/Carrinho.java
+++ src/main/java/br/com/caelum/ingresso/modelo/Carrinho.java
@@ -1,12 +1,15 @@
 package br.com.caelum.ingresso.modelo;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 
+import br.com.caelum.ingresso.model.Compra;
 import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Lugar;
 
 
 @Component
@@ -18,6 +21,13 @@ public class Carrinho {
 	public void add(Ingresso ingresso){
 		ingressos.add(ingresso);
 	}
+	
+	public Compra toCompra(){
+		return new Compra(ingressos);
+	}
+	
+	
+	
 
 	public List<Ingresso> getIngressos() {
 		return ingressos;
@@ -28,6 +38,12 @@ public class Carrinho {
 	}
 	
 	
+	public boolean isSelecionado(Lugar lugar){
+		return ingressos.stream().map(Ingresso::getLugar).anyMatch(l -> l.equals(lugar));
+	}
 	
+	public BigDecimal getTotal(){
+		return ingressos.stream().map(Ingresso::getPreco).reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
+	}
 
 }
